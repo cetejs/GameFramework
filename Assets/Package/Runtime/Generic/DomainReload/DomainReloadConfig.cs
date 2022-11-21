ba4d467cbb7c69b8c1eb6f122ddd8d5af393ecd7
Xianjun Yin <cetejs@gmail.com>
2022-11-21 16:38:14 +0800

    add domain reload and fix code format

---
+++ Assets/Package/Runtime/Generic/DomainReload/DomainReloadConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFramework
+{
+    public class DomainReloadConfig : ScriptableObject
+    {
+        public List<string> reloadAssemblies = new List<string>();
+
+        private static DomainReloadConfig instance;
+        
+        public static DomainReloadConfig Get()
+        {
+            if (instance)
+            {
+                return instance;
+            }
+
+            DomainReloadConfig config = Resources.Load<DomainReloadConfig>("DomainReloadConfig");
+            if (!config)
+            {
+                Debug.LogError("Please press GameFramework/ImportConfig");
+                return null;
+            }
+
+            instance = config;
+            return instance;
+        }
+    }
+}
