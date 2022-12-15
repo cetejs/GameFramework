63951df99627b97cc4ff059a1f5a2bf3c62fe3d7
Xianjun Yin <cetejs@gmail.com>
2022-12-15 21:42:35 +0800

    add assembly utils

---
+++ Assets/Package/Runtime/Utils/AssemblyUtils.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace GameFramework.Utils
+{
+    public static class AssemblyUtils
+    {
+        private static List<Assembly> loadedAssemblies;
+        private static Dictionary<string, Type> typeLookup = new Dictionary<string, Type>();
+
+        public static Type GetType(string typeName)
+        {
+            if (loadedAssemblies == null)
+            {
+                loadedAssemblies = new List<Assembly>(AppDomain.CurrentDomain.GetAssemblies());
+            }
+
+            if (typeLookup.TryGetValue(typeName, out Type type))
+            {
+                return type;
+            }
+
+            foreach (Assembly assembly in loadedAssemblies)
+            {
+                type = assembly.GetType(typeName);
+                if (type != null)
+                {
+                    typeLookup.Add(typeName, type);
+                    return type;
+                }
+            }
+
+            return null;
+        }
+    }
+}
+
